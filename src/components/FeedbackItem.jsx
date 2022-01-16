6211e1693cc18954f980395049b0789ffc96cf45
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-01-16 18:12:57 +0100

    Delete item implemented

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,22 @@
-function FeedbackItem({item}) {
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
+
+function FeedbackItem({item, handleDelete}) {
 
     return (
-        <div className="card">
+        <Card>
             <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className="close">
+                <FaTimes color='#0A4958'/>
+            </button>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
